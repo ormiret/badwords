875c5737c172b542e78cbbf890edfd60474e322f
Robert McWilliam <rmcw@allmail.net>
2015-02-08 22:55:10 +0000

    Modify mergelists.py to use sets and with.

--- mergelists.py
+++ mergelists.py
@@ -1,30 +1,19 @@
+#!/usr/bin/env python
 #word compile and sort
-#usage = word-merge.py firstlistfile secondlistfile
+#usage: mergelists.py firstlistfile secondlistfile
 import sys
 
-first = open(sys.argv[1],'r')
-first_file = first.readlines()
-first.close()
+with open(sys.argv[1],'r') as f:
+	first = f.readlines()
 
-second = open(sys.argv[2],'r')
-second_file = second.readlines()
-second.close()
+with open(sys.argv[2],'r') as f:
+	second = f.readlines()
 
-new_list = []
-
-for x in first_file:
-	if x not in new_list:
-		new_list.append(x)
-
-for x in second_file:
-	if x not in new_list:
-		new_list.append(x)
-
-final_list = sorted(new_list)
+final_list = sorted(list(set(first) | set(second)))
 
 new_file_name = sys.argv[1].replace('.txt','') + "-" + sys.argv[2].replace('.txt','') + ".txt"
 
-new_list_file = open(new_file_name,'w')
-for x in final_list:
-	new_list_file.write(x)
-new_list_file.close()
+with open(new_file_name,'w') as f:
+	[f.write(x) for x in final_list]
+
+
